12dcc4357ff8e879c2305cf6d5c4eb72b720b58f
marinaoleinik <marina.oleinik@tthk.ee>
2022-12-09 15:08:23 +0200

    Uncomm

--- TARpv22.py
+++ TARpv22.py
@@ -3,16 +3,16 @@ from math import * # * - kõik funktsioonid moodulist
 
 # 08/12/2022
 
-#print("Tere tulemast!") # info ekraanile, väljundlause
-#nimi=input("Mis sinu nimi on? ") #Sisend lugemine jaoks input()->str
-#print() #tühi rida
-#print(f"{nimi}, sul on väga ilus nimi!")
-#print(nimi,", sul on väga ilus nimi!") #,->tühik
-#print(nimi+", sul on väga ilus nimi!") # str+str 
-#vanus=int(input("Kui vana sa oled? ")) # int(str)
-#print(nimi,"sa oled",vanus,"aastat vana")
-#vanus+=10
-#print(nimi+", 10 aasta pärast sa oled"+str(vanus)+"aastat vana")
+print("Tere tulemast!") # info ekraanile, väljundlause
+nimi=input("Mis sinu nimi on? ") #Sisend lugemine jaoks input()->str
+print() #tühi rida
+print(f"{nimi}, sul on väga ilus nimi!")
+print(nimi,", sul on väga ilus nimi!") #,->tühik
+print(nimi+", sul on väga ilus nimi!") # str+str 
+vanus=int(input("Kui vana sa oled? ")) # int(str)
+print(nimi,"sa oled",vanus,"aastat vana")
+vanus+=10
+print(nimi+", 10 aasta pärast sa oled"+str(vanus)+"aastat vana")
 print()
 print("Võrdse pindalaga ristkülik ja ring")
 a=float(input("Anna laius: "))
