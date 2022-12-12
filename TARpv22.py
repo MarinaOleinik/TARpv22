6d7742dd1d4371d520c79ee96c29258d48f1c6d8
marinaoleinik <marina.oleinik@tthk.ee>
2022-12-12 13:36:19 +0200

    #12/12/22

--- TARpv22.py
+++ TARpv22.py
@@ -1,5 +1,83 @@
 from math import * # * - kõik funktsioonid moodulist 
+from random import *
+from stringprep import c22_specials
+from tkinter import Pack
 #import math      math.funktsioon()
+#12/12/22
+#1.
+print("Puu läbimõõdu arvutamine")
+#Kirjuta programm, mis küsib kasutaja käest puu ümbermõõdu ning teatab selle peale puu läbimõõdu.
+C=float(input("Anna ümbermõõt: "))
+d=round(C/pi,2)
+print(f"Puu läbimõõt = {d}")
+
+#2.
+#Arvutage Pythoni käsureal, kui pikk on ristkülikukujulise maatüki diagonaal, mille mõõtmed on Nm x Mm. N ja M küsi kasutajalt.
+N=float(input("Sisesta N: "))
+M=float(input("Sisesta M: "))
+d=round(sqrt(N**2+M**2),2)
+print(f"Ristkülikukujulise maatüki diagonaal on {d}")
+
+#3
+aeg = float(input("Mitu tundi kulus sõiduks? "))
+teepikkus = float(input("Mitu kilomeetrit sõitsid? "))
+kiirus = teepikkus/aeg
+
+print("Sinu kiirus oli " + str(kiirus) + " km/h")
+
+#4
+print("Aritmeetiline keskmine")
+A1=int(input("Esimene arv: "))
+A2=int(input("Teine arv: "))
+A3=int(input("Kolmas arv: "))
+A4=int(input("Neljas arv: "))
+A5=int(input("Viies arv: "))
+K=(A1+A2+A3+A4+A5)/5
+print(f"Kesknime on {K}")
+
+#5
+print("Joonista samasugune konn")
+print("    @..@")
+print("   (----)") 
+print("  ( \__/ )")
+print("^^ "'""'" ^^")  
+
+#6
+a=randint(0,100)
+b=randint(0,100)
+c=randint(0,100)
+print(f"a={a}\n,b={b}\n,c={c}")
+P=a+b+c
+print(f"Ümbermõõt on {P}")
+
+#7
+P=randint(1,6)
+summa=(12.9*1.1)/P
+print(f"{P}-st, Igaüks maksab {summa} ")
+
+#8
+print("Kütusekulu arvutamine")
+l=float(input("Kütuse liitride kogus: "))
+km=float(input("Läbitud kilomeetrid: "))
+kulu=(l/km)*100
+print(f"Kütusekulu {kulu}")
+
+#9
+print("Rulluisutajad")
+M=int(input("Minutid:"))
+M=M/60
+tee=M*29.9
+print(f"Jõuab {tee} km")
+
+#10
+print("Ajateisendus")
+M=int(input("Sisesta aja minutites")) #1h=60min
+H=M//60 #h
+M=M%60  #min
+print(f"{H}:{M}")
+
+
+
 
 # 08/12/2022
 
