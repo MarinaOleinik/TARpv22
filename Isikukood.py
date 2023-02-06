de038439caeeb54412efa6b3d000d1c35b8d8497
marina.oleinik <marina.oleinik@HP-CZC2349HSZ>
2023-02-06 15:45:49 +0200

    sort on lisatud

--- Isikukood.py
+++ Isikukood.py
@@ -1,16 +1,33 @@
 from xmlrpc.client import DateTime
 from OmaMoodul import *
 ikood=""
+arvud=["100","1001","211","222"]
+ikoodid=["37702200310","47610112243","60201030210"]
+
 while True:
     ikood=input("Sisesta isikukood: ")
+    if ikood=="-": break
     if pikkus(ikood)==False:
         print("Liiga pikk või lühike isikukood")
+        arvud.append(ikood)
     else:
         s=sugu(ikood)
+        
         if s=="viga":
             print("Esimene täht ei ole õige")
+            arvud.append(ikood)
         else:
             if sunnipaev(ikood)=="viga":
                 print("2-7 tähed on valesti sisestatud")
             else:
                 lause(ikood)
+                if kontrollnr(ikood)==int(ikood[-1]):
+                    print("OK")
+                    ikoodid.append(ikood)
+                else:
+                    print("!!!")
+print()
+ikoodid=naised_mehed(ikoodid)
+print(ikoodid)
+arvud=arvud_sorted(arvud)
+print(arvud)
--- OmaMoodul.py
+++ OmaMoodul.py
@@ -60,3 +60,43 @@ def sunnikoht(ikood: str)->str:
 
 def lause(ikood: str)->str:
     print(f"See on {sugu(ikood)} ta on sündinud {sunnipaev(ikood)}, tema sünnikoht on {sunnikoht(ikood)}")
+
+def kontrollnr(ikood:str)->int:
+    astme1=[1,2,3,4,5,6,7,8,9,1]
+    astme2=[3,4,5,6,7,8,9,1,2,3]
+    ik_list=list(ikood)
+    ik_list=list(map(int,ik_list))
+    summa=0
+    for i in range(0,10,1):
+        summa+=ik_list[i]*astme1[i]
+    s=(summa//11)*11
+    jaak=summa-s
+    if jaak==int(ik_list[10]):
+        return jaak
+    elif jaak==10:
+        return 0
+    else:
+        summa=0
+        for i in range(0,10,1):
+            summa+=ik_list[i]*astme2[i]
+        s=(summa//11)*11
+        jaak=summa-s
+        return jaak
+
+def naised_mehed(ikoodid:list)->list:
+    naised=[]
+    mehed=[]
+    for kood in ikoodid:
+        kood_=list(kood)
+        if int(kood_[0])%2==0:
+            naised.append(kood)
+        else:
+            mehed.append(kood)
+    naised.extend(mehed)
+    ikoodid=naised
+    return ikoodid
+
+def arvud_sorted(arvud:list)->list:
+    arvud=list(map(int,arvud)) #[111,3333,2222,3444,222,7]
+    arvud.sort()
+    return arvud
